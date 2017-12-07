38d07b032e4f0ffbec6b5f31f2df467f42eb8f21
程召华 <chengzhaohua@chengzhaohuadeMacBook-Pro.local>
2017-12-07 16:24:45 +0800

    保存

--- CZHAddressPickerView/ViewController.m
+++ CZHAddressPickerView/ViewController.m
@@ -25,7 +25,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-//省份，
+/**
+ * 只显示省份一级
+ * provinceBlock : 回调省份
+ */
 - (IBAction)provinceWithoutInput:(id)sender {
     
  
@@ -36,7 +39,11 @@
     
     
 }
-
+/**
+ * 只显示省份一级
+ * province : 传入了省份自动滚动到省份，没有传或者找不到默认选中第一个
+ * provinceBlock : 回调省份
+ */
 - (IBAction)provinceWithInput:(id)sender {
     
     CZHWeakSelf(self);
@@ -44,13 +51,15 @@
         CZHStrongSelf(self);
         self.province = province;
         
-        
         [sender setTitle:[NSString stringWithFormat:@"%@",province] forState:UIControlStateNormal];
     }];
     
 }
 
-
+/**
+ * 显示省份和市级
+ * cityBlock : 回调省份和城市
+ */
 - (IBAction)cityWithoutInput:(id)sender {
     
     [CZHAddressPickerView cityPickerViewWithCityBlock:^(NSString *province, NSString *city) {
@@ -58,7 +67,11 @@
     }];
     
 }
-
+/**
+ * 显示省份和市级
+ * province,city : 传入了省份和城市自动滚动到选中的，没有传或者找不到默认选中第一个
+ * cityBlock : 回调省份和城市
+ */
 - (IBAction)cityWithInput:(id)sender {
     CZHWeakSelf(self);
     [CZHAddressPickerView cityPickerViewWithProvince:self.province city:self.city cityBlock:^(NSString *province, NSString *city) {
@@ -70,13 +83,21 @@
     }];
 }
 
+/**
+ * 显示省份和市级和区域
+ * areaBlock : 回调省份城市和区域
+ */
 - (IBAction)areaWithoutInput:(id)sender {
     
     [CZHAddressPickerView areaPickerViewWithAreaBlock:^(NSString *province, NSString *city, NSString *area) {
         [sender setTitle:[NSString stringWithFormat:@"%@%@%@",province,city,area] forState:UIControlStateNormal];
     }];
 }
-
+/**
+ * 显示省份和市级和区域
+ * province,city : 传入了省份和城市和区域自动滚动到选中的，没有传或者找不到默认选中第一个
+ * areaBlock : 回调省份城市和区域
+ */
 - (IBAction)areaWithInput:(id)sender {
     CZHWeakSelf(self);
     [CZHAddressPickerView areaPickerViewWithProvince:self.province city:self.city area:self.area areaBlock:^(NSString *province, NSString *city, NSString *area) {
